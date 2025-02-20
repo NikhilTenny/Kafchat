86e98a7fe01fe8de5c7114b49de5c03122bac061
NikhilTenny <nikhiltenny19@gmail.com>
2025-02-20 16:08:48 +0530

    removed duplicate files

--- frontend/my-angular-project/src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-
-export const routes: Routes = [
-    { path: '/sign-up',loadChildren: () => import('./components/signup/signup.component').then(m => m.SignupComponent) }
-
-];
